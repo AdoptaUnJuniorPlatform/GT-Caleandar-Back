9b13c4eb06319984177a669c77d3a8dc529d18d9
Jorge Guillen <ashgard2@gmail.com>
2024-11-23 13:57:40 +0100

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
 // functions/index.js
 const functions = require('firebase-functions');
 const cors = require('./middleware/corsMiddleware');
-const { endDatos, getTasksByUserId, updateTask, verifyDailyTasks, cleanupOldTasks } = require('./handlers/taskHandler');
+const { endDatos, getTasksByUserId, updateTask, } = require('./handlers/taskHandler');
+const { verifyDailyTasks, cleanupOldTasks } = require('./handlers/reminderHandler');
+
 
 // Usando CORS middleware en las funciones HTTP
 exports.endDatos = functions.https.onRequest((req, res) => cors(req, res, () => endDatos(req, res)));
 exports.getTasksByUserId = functions.https.onRequest((req, res) => cors(req, res, () => getTasksByUserId(req, res)));
+
 // Aquí usamos onCall para la función de actualización de tarea
 exports.updateTask = functions.https.onCall(updateTask);  // Esta función es callable, por lo tanto, no se usa con req/res
 exports.verifyDailyTasks = functions.https.onRequest((req, res) => cors(req, res, () => verifyDailyTasks(req, res)));
